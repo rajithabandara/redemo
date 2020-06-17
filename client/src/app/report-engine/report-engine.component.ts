a383e53cb22a0088be17be99cd809fe578056c36
devindianushika <devindianushika@gmail.com>
2020-06-17 09:27:03 +0530

    Added switch case statement

--- client/src/app/report-engine/report-engine.component.ts
+++ client/src/app/report-engine/report-engine.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SvgcircleComponent } from '../components/svgcircle/svgcircle.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-report-engine',
@@ -12,7 +13,18 @@ export class ReportEngineComponent implements OnInit {
     { id: 1, name: 'Ready' },
     { id: 2, name: 'Started' },
   ];
-  constructor() {}
 
+
+  qparam:any;
+  constructor(router:ActivatedRoute) {
+    router.queryParamMap.subscribe(data =>{
+      this.qparam = data.get('item');
+      console.log(this.qparam);
+    })
+  }
+
+
+  
   ngOnInit(): void {}
 }
+
