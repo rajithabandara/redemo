bd3f6fa5a1275bea5bad1827b57d99656b534eac
devindianushika <devindianushika@gmail.com>
2020-06-17 08:38:22 +0530

    Modified report-engine component

--- client/src/app/components/report-engine/report-engine.component.ts
+++ client/src/app/components/report-engine/report-engine.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-report-engine',
@@ -6,8 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./report-engine.component.css']
 })
 export class ReportEngineComponent implements OnInit {
+  qparam:any;
+//   plain:any;
+//  circle:any;
+//  rectan:any;
+//  ellipse:any;
 
-  constructor() { }
+  constructor(router:ActivatedRoute) {
+
+    router.queryParamMap.subscribe(data =>{
+      this.qparam = data.get('item');
+      console.log(this.qparam);
+
+      // switch (this.qparam) {
+      //   case "circle":
+      //       console.log("It is a Sunday.");
+      //       break;
+      //   case "plain":
+      //       console.log("It is a Monday.");
+      //       break;
+      //   case "rectan":
+      //       console.log("It is a Tuesday.");
+      //       break;
+      //       default:
+      //         console.log("No such day exists!");
+      //         break;
+      // }
+        })
+
+   }
 
   ngOnInit(): void {
   }
