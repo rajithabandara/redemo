8106ea616b73a20271568170520742bd6c7afba8
rajithabandara <rajithag.17@itfac.mrt.ac.lk>
2020-06-22 10:10:03 +0530

    added center func demo

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ReportEngineComponent } from './components/report-engine/report-engine.component';
-
-
+import { CenterdemoComponent } from './components/centerdemo/centerdemo.component';
 
 const routes: Routes = [
-  { path: 'reportengine/:id', component:ReportEngineComponent},
-
+  { path: 'reportengine/:id', component: ReportEngineComponent },
+  { path: 'centerdemo', component: CenterdemoComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- client/src/app/components/centerdemo/centerdemo.component.ts
+++ client/src/app/components/centerdemo/centerdemo.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-centerdemo',
   templateUrl: './centerdemo.component.html',
-  styleUrls: ['./centerdemo.component.css']
+  styleUrls: ['./centerdemo.component.css'],
 })
 export class CenterdemoComponent implements OnInit {
+  x: number;
+  y: number;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.x = Math.round((Math.random() * 5000) % 500);
+    this.y = Math.round((Math.random() * 5000) % 500);
   }
 
+  ngOnInit(): void {
+    console.log(this.x, ' ', this.y);
+  }
+
+  center() {
+    this.x = 400;
+    this.y = 300;
+
+    console.log('center - ', this.x, ' ', this.y);
+  }
 }
