b8a977359ae5b0a22a1c5e10c22bc90dfb581e0b
rajithabandara <rajithag.17@itfac.mrt.ac.lk>
2020-06-22 09:54:05 +0530

    added centerdemo component

---
+++ client/src/app/components/centerdemo/centerdemo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-centerdemo',
+  templateUrl: './centerdemo.component.html',
+  styleUrls: ['./centerdemo.component.css']
+})
+export class CenterdemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
