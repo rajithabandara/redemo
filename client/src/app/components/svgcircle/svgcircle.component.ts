838d91673917a8f8d4aef0ba54bea29b6d757a17
devindianushika <devindianushika@gmail.com>
2020-06-22 09:58:46 +0530

    refactor svgcircle  and svgrect components

--- client/src/app/components/svgcircle/svgcircle.component.ts
+++ client/src/app/components/svgcircle/svgcircle.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import { HttpdataService } from 'src/app/services/httpdata.service';
 
 
 
@@ -11,9 +11,30 @@ import { Component, OnInit, Input } from '@angular/core';
 export class SvgcircleComponent implements OnInit {
   @Input()
   coordinates: any;
-  constructor() {}
+  public cirCoordinates = [];
+  timer = null;
+  constructor(private httpdata: HttpdataService) {}
 
   ngOnInit(): void {
     console.log(this.coordinates);
+    this.createCir();
   }
+  
+createCir(){
+  clearInterval(this.timer);
+  this.httpdata.getRandomInt().subscribe((data) => {
+    console.log(data);
+    this.cirCoordinates = [];
+    for (let i = 0; i < data.randomint; i++) {
+      let x = (Math.random() * 5000) % 500;
+      let y = (Math.random() * 5000) % 500;
+
+      this.cirCoordinates.push({
+        x: x,
+        y: y,
+      });
+    }
+  });
+}
+
 }
