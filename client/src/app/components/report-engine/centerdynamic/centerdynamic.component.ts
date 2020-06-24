cb49e04493614a15c774675689071319380a0072
rajithabandara <rajithag.17@itfac.mrt.ac.lk>
2020-06-24 12:35:40 +0530

    refactor , break into methods , added calc service

--- client/src/app/components/report-engine/centerdynamic/centerdynamic.component.ts
+++ client/src/app/components/report-engine/centerdynamic/centerdynamic.component.ts
@@ -1,46 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { CalcService } from '../../../services/calc.service';
 
 @Component({
   selector: 'app-centerdynamic',
   templateUrl: './centerdynamic.component.html',
-  styleUrls: ['./centerdynamic.component.css']
+  styleUrls: ['./centerdynamic.component.css'],
 })
 export class CenterdynamicComponent {
   coordinates = [];
+
   toggle: boolean = false;
+
   atrstr = ``;
-  viewboxstr = ``;
 
-  viwporth: number;
-  viwportw: number;
+  viewBoxAttribute = ``;
 
-  constructor() {
+  reportHeight: number;
+
+  reportWidth: number;
+
+  calculateService: CalcService;
+
+  constructor(calculateService: CalcService) {
+    this.calculateService = calculateService;
     for (let i = 0; i < 20; i++) {
       this.coordinates.push({
-        // x: 500,
-        // y: 500,
-        x: Math.round((Math.random() * 5000) % 1000),
-        y: Math.round((Math.random() * 5000) % 1000),
+        x: Math.round((Math.random() * 5000) % 4000),
+        y: Math.round((Math.random() * 5000) % 4000),
       });
     }
 
-    let harr = this.coordinates.map((e) => e.y);
+    let reportDimesion = calculateService.calculateReportSize(this.coordinates);
 
-    let warr = this.coordinates.map((e) => e.x);
+    console.log(reportDimesion);
 
-    harr.sort();
-    warr.sort();
+    this.reportHeight = reportDimesion.reportHeight;
+    this.reportWidth = reportDimesion.reportWidth;
 
-    this.viwporth = 4000; //harr[harr.length - 1] + 300;
-    this.viwportw = 4000; //warr[warr.length - 1] + 300;
+    // this.reportHeight = 4000;
+    // this.reportWidth = 4000;
 
-    console.log(this.viwportw, this.viwporth);
+    // console.log(this.reportWidth, this.reportHeight);
 
-    // this.viewboxstr = `0 0 2400 2400`;
+    // this.viewBoxAttribute = `0 0 2400 2400`;
 
-    this.viewboxstr = `0 0  ${this.viwportw} ${this.viwporth}`;
-
-    console.log(harr, warr);
+    this.viewBoxAttribute = `0 0  ${this.reportWidth} ${this.reportHeight}`;
   }
 
   center() {
@@ -49,55 +53,13 @@ export class CenterdynamicComponent {
     console.log(this.toggle, ' toggled !');
 
     if (!this.toggle) {
-      let el: any = document.querySelector('#groptsvg');
-      // let midX =
-      //   (el.getBoundingClientRect().left + el.getBoundingClientRect().right) /
-      //   2;
-      // let midY =
-      //   (el.getBoundingClientRect().top + el.getBoundingClientRect().bottom) /
-      //   2;
+      let grouppElement: any = document.querySelector('#groptsvg');
 
-      // console.log('g top - ', el.getBBox());
-
-      // console.log('*****************************');
-
-      // console.log('group center - ', midX, midY);
-      // console.log(
-      //   ' svg canvas center - ',
-      //   this.viwportw / 2,
-      //   this.viwporth / 2
-      // );
-
-      // let mainsvg = document.getElementById('mainsvg');
-
-      console.log(el.getBBox());
-
-      // height: 200;
-      // width: 200;
-      // x: 500;
-      // y: 500;
-
-      let midx = el.getBBox().width / 2 + el.getBBox().x;
-      let midy = el.getBBox().height / 2 + el.getBBox().y;
-
-      let diffx = this.viwportw / 2 - midx;
-      let diffy = this.viwporth / 2 - midy;
-
-      console.log('diff - ', diffx, diffy);
-
-      this.atrstr = `translate(${diffx},${diffy})`;
+      this.atrstr = this.calculateService.calculateCenter(
+        grouppElement.getBBox(),
+        this.reportWidth,
+        this.reportHeight
+      );
     }
-
-    // var  ctm = document.getElementById("object_7").getCTM()
-
-    // // Calculate the centre of the group
-    // var cx = bbox.x + bbox.width/2;
-    // var cy = bbox.y + bbox.height/2;
-
-    // // Transform cx,cy by the group's transform
-    // var pt = document.getElementById("mysvg").createSVGPoint();
-    // pt.x = cx;
-    // pt.y = cy;
-    // pt = pt.matrixTransform(ctm);
   }
 }
