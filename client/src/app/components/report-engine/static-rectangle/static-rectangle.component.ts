753b9f21d50c1d111c73506ba36cb664c2a9630d
rajithabandara <rajithag.17@itfac.mrt.ac.lk>
2020-06-24 15:54:57 +0530

    minor changes

--- client/src/app/components/report-engine/static-rectangle/static-rectangle.component.ts
+++ client/src/app/components/report-engine/static-rectangle/static-rectangle.component.ts
@@ -3,18 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-static-rectangle',
   templateUrl: './static-rectangle.component.html',
-  styleUrls: ['./static-rectangle.component.css']
+  styleUrls: ['./static-rectangle.component.css'],
 })
 export class StaticRectangleComponent {
-
   x: number;
   y: number;
 
-  constructor() {
+  translateArrt = ``;
 
+  constructor() {
+    this.x = 300;
+    this.y = 500;
   }
   center() {
-    this.x = 1000;
-    this.y = 800;
+    this.translateArrt = `translate( 200 , 250 )`;
   }
 }
