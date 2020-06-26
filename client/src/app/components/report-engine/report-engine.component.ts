c48a85af93add943a6a5f7295ba34afbd6cb285d
rajithabandara <rajithag.17@itfac.mrt.ac.lk>
2020-06-26 12:21:17 +0530

    added centering

--- client/src/app/components/report-engine/report-engine.component.ts
+++ client/src/app/components/report-engine/report-engine.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
-
-
+import { CalcService } from '../../services/calc.service';
 
 @Component({
   selector: 'app-report-engine',
@@ -10,16 +8,55 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./report-engine.component.css'],
 })
 export class ReportEngineComponent {
-
   param: any;
+  translateAttribute = ``;
+  reportWidth: number;
+  reportHeight: number;
+  toggle: boolean = false;
+  rectcoordx: number;
+  rectcoordy: number;
 
-  constructor(private router: ActivatedRoute ) {
+  calculateService: CalcService;
+
+  constructor(private router: ActivatedRoute, calculateService: CalcService) {
+    this.calculateService = calculateService;
+
     this.router.paramMap.subscribe((data) => {
       this.param = data.get('id');
       console.log(this.param);
-
     });
   }
 
-  ngOnInit(): void { }
+  center() {
+    console.log(this.toggle, ' toggled !');
+
+    if (!this.toggle) {
+      let svgarea: any = document.getElementById('svgarea');
+
+      let bbox = svgarea.getBoundingClientRect();
+
+      let rectbbox = svgarea.getBBox();
+
+      let midx = bbox.width / 2;
+
+      this.reportHeight = bbox.height;
+      this.reportWidth = bbox.width;
+
+      this.rectcoordx = this.reportWidth / 2 - rectbbox.width / 2;
+      this.rectcoordy = this.reportHeight / 2 - rectbbox.height / 2;
+
+      console.log(rectbbox.height, rectbbox.width);
+
+      // this.translateAttribute = `translate( ${tx} , 0 )`;
+
+      // this.translateAttribute = this.calculateService.calculateCenter(
+      //   rectbbox,
+      //   this.reportWidth,
+      //   this.reportHeight
+      // );
+    } else {
+      this.translateAttribute = `translate( 0 , 0 )`;
+    }
+    this.toggle = !this.toggle;
+  }
 }
