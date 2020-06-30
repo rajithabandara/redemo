95c35fca57f075eacd5a4d1a8b10ed5fba2afe63
devindianushika <devindianushika@gmail.com>
2020-06-30 12:20:31 +0530

    Added roter paths

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ReportEngineComponent } from './components/report-engine/report-engine.component';
+import { PlainSvgComponent } from './components/plain-svg/plain-svg.component';
+import { SvgcircleComponent } from './components/svgcircle/svgcircle.component';
+import { SvgrectComponent } from './components/svgrect/svgrect.component';
+import { SvgelipseComponent } from './components/svgelipse/svgelipse.component';
 
 const routes: Routes = [
-  { path: 'reportengine/:id', component: ReportEngineComponent },
+  { path: 'reportengine', component: ReportEngineComponent,  children:[
+    { path: 'Plainsvg', component: PlainSvgComponent },
+    { path: 'circles', component:SvgcircleComponent },
+    { path: 'rectangles', component:SvgrectComponent },
+    { path: 'ellipse', component: SvgelipseComponent },
+  ] },
 ];
 
 @NgModule({
