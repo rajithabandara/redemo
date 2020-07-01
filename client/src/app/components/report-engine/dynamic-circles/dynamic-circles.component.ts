1ac20ef61b2a149da61e682149d7e6cf7d9c80e8
rajithabandara <rajithag.17@itfac.mrt.ac.lk>
2020-07-01 10:26:30 +0530

    added dynamic circles and rectangles

--- client/src/app/components/report-engine/dynamic-circles/dynamic-circles.component.ts
+++ client/src/app/components/report-engine/dynamic-circles/dynamic-circles.component.ts
@@ -11,11 +11,15 @@ export class DynamicCirclesComponent implements OnInit {
   translateAttribute = ``;
   viewableAreaWidth = 0;
   viewableAreaHeight = 0;
+  coordinates = [];
 
   constructor(private sharedService: SharedService) {
-
-    
-
+    for (let i = 0; i < 20; i++) {
+      this.coordinates.push({
+        x: Math.round((Math.random() * 5000) % 1000),
+        y: Math.round((Math.random() * 5000) % 1000),
+      });
+    }
   }
   ngOnInit(): void {
     this.sharedService.sharedMessage.subscribe((translationDataStr) => {
--- client/src/app/components/report-engine/dynamic-rectangles/dynamic-rectangles.component.ts
+++ client/src/app/components/report-engine/dynamic-rectangles/dynamic-rectangles.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from '../../../services/sharedData.service';
 
 @Component({
   selector: 'app-dynamic-rectangles',
   templateUrl: './dynamic-rectangles.component.html',
-  styleUrls: ['./dynamic-rectangles.component.css']
+  styleUrls: ['./dynamic-rectangles.component.css'],
 })
 export class DynamicRectanglesComponent implements OnInit {
+  translateAttribute = ``;
+  viewableAreaWidth = 0;
+  viewableAreaHeight = 0;
+  coordinates = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private sharedService: SharedService) {
+    for (let i = 0; i < 20; i++) {
+      this.coordinates.push({
+        x: Math.round((Math.random() * 5000) % 1000),
+        y: Math.round((Math.random() * 5000) % 1000),
+      });
+    }
   }
+  ngOnInit(): void {
+    this.sharedService.sharedMessage.subscribe((translationDataStr) => {
+      console.log(translationDataStr);
 
+      let translationData = JSON.parse(translationDataStr);
+
+      this.translateAttribute = translationData.translateAttribute;
+      this.viewableAreaWidth = translationData.viewableAreaWidth;
+      this.viewableAreaHeight = translationData.viewableAreaHeight;
+    });
+  }
 }
