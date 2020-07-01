6c3910e895092e19f24de4c5395cc86157ada743
rajithabandara <rajithag.17@itfac.mrt.ac.lk>
2020-07-01 10:17:35 +0530

    added dynamic circle component

--- client/src/app/components/report-engine/dynamic-circles/dynamic-circles.component.ts
+++ client/src/app/components/report-engine/dynamic-circles/dynamic-circles.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SharedService } from '../../../services/sharedData.service';
+
 @Component({
   selector: 'app-dynamic-circles',
   templateUrl: './dynamic-circles.component.html',
-  styleUrls: ['./dynamic-circles.component.css']
+  styleUrls: ['./dynamic-circles.component.css'],
 })
 export class DynamicCirclesComponent implements OnInit {
+  translateAttribute = ``;
+  viewableAreaWidth = 0;
+  viewableAreaHeight = 0;
 
-  constructor() { }
+  constructor(private sharedService: SharedService) {
 
-  ngOnInit(): void {
+    
+
   }
+  ngOnInit(): void {
+    this.sharedService.sharedMessage.subscribe((translationDataStr) => {
+      console.log(translationDataStr);
 
+      let translationData = JSON.parse(translationDataStr);
+
+      this.translateAttribute = translationData.translateAttribute;
+      this.viewableAreaWidth = translationData.viewableAreaWidth;
+      this.viewableAreaHeight = translationData.viewableAreaHeight;
+    });
+  }
 }
