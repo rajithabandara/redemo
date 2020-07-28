78ad58499a4da60306c07f0265f61ad17beb871c
Rajitha Gunathilake <RajithaG@embla.lk>
2020-07-28 17:07:47 +0530

    added centering

--- client/src/app/components/report-engine/dynamicloader/dynamicloader.component.ts
+++ client/src/app/components/report-engine/dynamicloader/dynamicloader.component.ts
@@ -6,30 +6,41 @@ import {
   ViewContainerRef,
   ComponentFactoryResolver,
   ComponentRef,
+  ElementRef,
+  AfterViewInit,
 } from '@angular/core';
 import { DynamicviewrefDirective } from 'src/app/directives/dynamicviewref.directive';
 import { Dynamiccomp1Component } from '../dynamiccomp1/dynamiccomp1.component';
 import { Dynamiccomp2Component } from '../dynamiccomp2/dynamiccomp2.component';
 import { AdComponent } from '../../ad.AdComponent';
+import { Rectangle1Component } from '../rectangle1/rectangle1.component';
 
 @Component({
   selector: 'app-dynamicloader',
   templateUrl: './dynamicloader.component.html',
   styleUrls: ['./dynamicloader.component.css'],
 })
-export class DynamicloaderComponent implements OnInit {
+export class DynamicloaderComponent implements OnInit, AfterViewInit {
   // @ViewChild('container', { read: ViewContainerRef })
   // viewContainerRef: ViewContainerRef;
 
   @ViewChild(DynamicviewrefDirective, { static: true })
   dynamicDirective: DynamicviewrefDirective;
+  translateAttribute: string;
+  viewableAreaHeight: any;
+  viewableAreaWidth: any;
 
-  constructor(private componentFactoryResolver: ComponentFactoryResolver) {}
+  constructor(
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private hostElement: ElementRef
+  ) {}
 
   title = 'saadfkhjaslfjknshfb';
   ref;
   adItem;
   ngOnInit(): void {
+    this.adItem = { data: { name: this.title + 'askejflekjsjksldjkfj' } };
+
     // const viewContainerRef = this.dynamicDirective.vcr;
     // this.loaddynamicservice.isLoggedIn$
     //   .pipe(
@@ -41,16 +52,29 @@ export class DynamicloaderComponent implements OnInit {
     //   .subscribe();
 
     setInterval(() => {
-      this.title = Math.random().toString();
-      console.log(this.title);
+      // this.title = Math.random().toString();
+      // console.log(this.title);
       this.adItem = { data: { name: this.title + 'askejflekjsjksldjkfj' } };
 
-      if (this.ref) {
-        (<AdComponent>this.ref.instance).data = this.adItem.data;
-      }
+      // if (this.ref) {
+      //   (<AdComponent>this.ref.instance).data = this.adItem.data;
+      // }
     }, 200);
   }
+  ngAfterViewInit(): void {
+    console.log('afterviewinit');
 
+    let svgViewableArea: any = this.hostElement.nativeElement.querySelector(
+      '#svgViewableArea'
+    );
+
+    let reportArea: any = this.hostElement.nativeElement.querySelector(
+      '#reportArea'
+    );
+
+    console.log(svgViewableArea);
+    console.log(reportArea);
+  }
   addEditor1() {
     const factory = this.componentFactoryResolver.resolveComponentFactory(
       Dynamiccomp1Component
@@ -68,12 +92,12 @@ export class DynamicloaderComponent implements OnInit {
     // this.ref = this.viewContainerRef.createComponent(factory);
     // (<DynamicloaderComponent>componentRef.instance).data = adItem.data;
     // ref.instance.title = this.title;
-    this.ref.changeDetectorRef.detectChanges();
+    // this.ref.changeDetectorRef.detectChanges();
   }
 
   addEditor2() {
     const factory = this.componentFactoryResolver.resolveComponentFactory(
-      Dynamiccomp2Component
+      Rectangle1Component
     );
     const viewContainerRef = this.dynamicDirective.vcr;
 
@@ -88,7 +112,7 @@ export class DynamicloaderComponent implements OnInit {
     // this.ref = this.viewContainerRef.createComponent(factory);
     // (<DynamicloaderComponent>componentRef.instance).data = adItem.data;
     // ref.instance.title = this.title;
-    this.ref.changeDetectorRef.detectChanges();
+    // this.ref.changeDetectorRef.detectChanges();
   }
 
   refresh() {
@@ -96,5 +120,81 @@ export class DynamicloaderComponent implements OnInit {
       data: { name: this.title + '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@' },
     };
     (<AdComponent>this.ref.instance).data = this.adItem.data;
+  }
+
+  center() {
+    let svgViewableArea: any = this.hostElement.nativeElement.querySelector(
+      '#svgViewableArea'
+    );
+
+    let reportArea: any = this.hostElement.nativeElement.querySelector(
+      '#reportArea'
+    );
+
+    console.log(svgViewableArea);
+
+    let {
+      viewableAreaHeight,
+      viewableAreaWidth,
+    } = this.getViewableAreaDimesion(svgViewableArea);
+
+    this.viewableAreaHeight = viewableAreaHeight;
+    this.viewableAreaWidth = viewableAreaWidth;
+
+    this.translateAttribute = this.getCenterTranslation(
+      reportArea.getBBox(),
+      this.viewableAreaWidth,
+      this.viewableAreaHeight
+    );
+
+    console.log(this.translateAttribute);
+
+    let translationData = {
+      translateAttribute: this.translateAttribute,
+      viewableAreaHeight: this.viewableAreaHeight,
+      viewableAreaWidth: this.viewableAreaWidth,
+    };
+
+    let translationDataStr = JSON.stringify(translationData);
+
+    console.log(translationData);
+
+    this.adItem = {
+      data: {
+        name: this.title + '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',
+        translateAttribute: this.translateAttribute,
+        viewableAreaHeight: this.viewableAreaHeight,
+        viewableAreaWidth: this.viewableAreaWidth,
+      },
+    };
+    (<AdComponent>this.ref.instance).data = this.adItem.data;
+
+    // this.sharedService.emmitTranslationString(translationDataStr);
+  }
+
+  getViewableAreaDimesion(svgViewableArea: any) {
+    let boundryBox = svgViewableArea.getBoundingClientRect();
+
+    let viewableAreaHeight = boundryBox.height;
+    let viewableAreaWidth = boundryBox.width;
+    return { viewableAreaHeight, viewableAreaWidth };
+  }
+
+  getCenterTranslation(
+    reportBoundryBox: any,
+    viewableAreaWidth: number,
+    viewableAreaHeight: number
+  ): string {
+    console.log(reportBoundryBox);
+
+    reportBoundryBox = reportBoundryBox;
+
+    let reportCenterX = reportBoundryBox.width / 2 + reportBoundryBox.x;
+    let reportCenterY = reportBoundryBox.height / 2 + reportBoundryBox.y;
+
+    let differanceX = viewableAreaWidth / 2 - reportCenterX;
+    let differanceY = viewableAreaHeight / 2 - reportCenterY;
+
+    return `translate (${differanceX},${differanceY})`;
   }
 }
--- client/src/app/components/report-engine/rectangle1/rectangle1.component.ts
+++ client/src/app/components/report-engine/rectangle1/rectangle1.component.ts
@@ -1,34 +1,60 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  AfterViewInit,
+  ElementRef,
+} from '@angular/core';
 import { SharedService } from '../../../services/sharedData.service';
+import { AdComponent } from '../../ad.AdComponent';
 
 @Component({
   selector: 'app-rectangle1',
   templateUrl: './rectangle1.component.html',
   styleUrls: ['./rectangle1.component.css'],
 })
-export class Rectangle1Component implements OnInit, OnDestroy {
+export class Rectangle1Component
+  implements OnInit, OnDestroy, AfterViewInit, AdComponent {
   translateAttribute = ``;
   viewableAreaWidth = 0;
   viewableAreaHeight = 0;
 
   sharedServiceObservable: any;
 
-  constructor(private sharedService: SharedService) {}
+  constructor(
+    private sharedService: SharedService,
+    private hostElement: ElementRef
+  ) {}
+  ngAfterViewInit(): void {
+    console.log('afterviewinit');
+
+    let svgViewableArea: any = this.hostElement.nativeElement.querySelector(
+      '#svgViewableArea'
+    );
+
+    let reportArea: any = this.hostElement.nativeElement.querySelector(
+      '#reportArea'
+    );
+
+    console.log(svgViewableArea);
+    console.log(reportArea);
+    console.log(reportArea.getBBox());
+    // reportArea.getBBox(),
+  }
+  data: any;
   ngOnDestroy(): void {
     console.log('unsubscribe');
-    this.sharedServiceObservable.unsubscribe();
+    // this.sharedServiceObservable.unsubscribe();
   }
   ngOnInit(): void {
-    this.sharedServiceObservable = this.sharedService.sharedMessage.subscribe(
-      (translationDataStr) => {
-        console.log(translationDataStr);
-
-        let translationData = JSON.parse(translationDataStr);
-
-        this.translateAttribute = translationData.translateAttribute;
-        this.viewableAreaWidth = translationData.viewableAreaWidth;
-        this.viewableAreaHeight = translationData.viewableAreaHeight;
-      }
-    );
+    // this.sharedServiceObservable = this.sharedService.sharedMessage.subscribe(
+    //   (translationDataStr) => {
+    //     console.log(translationDataStr);
+    //     let translationData = JSON.parse(translationDataStr);
+    //     this.translateAttribute = translationData.translateAttribute;
+    //     this.viewableAreaWidth = translationData.viewableAreaWidth;
+    //     this.viewableAreaHeight = translationData.viewableAreaHeight;
+    //   }
+    // );
   }
 }
